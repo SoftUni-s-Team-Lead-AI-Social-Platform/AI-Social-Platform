da94f45610c6f4febcc69175c8cf858e4ae40126
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-14 22:12:52 +0200

    Add Login component

--- ai-social-platform.client/src/App.jsx
+++ ai-social-platform.client/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
             <main className="main-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </main>
 
