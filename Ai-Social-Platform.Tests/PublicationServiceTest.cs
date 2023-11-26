e846f7a8a3c58c36bb241b230d60f1799b58d7ca
Bagie <Bagriyan.dilyanov@abv.bg>
2023-11-26 14:28:08 +0200

    Added tests for each service method

--- Ai-Social-Platform.Tests/PublicationServiceTest.cs
+++ Ai-Social-Platform.Tests/PublicationServiceTest.cs
@@ -33,7 +33,7 @@ namespace Ai_Social_Platform.Tests
 
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.NameIdentifier, "user@user.com")
+                new Claim(ClaimTypes.NameIdentifier, "user@test.com")
             };
 
             var identity = new ClaimsIdentity(claims, "TestAuthType");
@@ -179,13 +179,296 @@ namespace Ai_Social_Platform.Tests
             {
                 Content = "This is a test publication"
             };
-            var publication = dbContext.Publications.First(p => p.AuthorId.ToString() == "6d5800ce-d726-4fc8-83d9-d6b3ac1f591e");
+            var publication =
+                dbContext.Publications.First(p => p.AuthorId.ToString() == "123400ce-d726-4fc8-83d9-d6b3ac1f591e");
 
             // Act
             await publicationService.UpdatePublicationAsync(dto, publication.Id);
 
             // Assert
             Assert.That(publication.Content, Is.EqualTo(dto.Content));
+        }
+
+        [Test]
+        public async Task UpdatePublicationAsync_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var dto = new PublicationFormDto()
+            {
+                Content = "This is a test publication"
+            };
+            var invalidPublicationId = Guid.NewGuid(); // Use a non-existing ID
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<NullReferenceException>(async () =>
+            {
+                await publicationService.UpdatePublicationAsync(dto, invalidPublicationId);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(PublicationNotFound));
+        }
+
+
+        [Test]
+        public async Task UpdatePublicationAsync_ThrowsAccessViolationExceptionException()
+        {
+            // Arrange
+            var dto = new PublicationFormDto()
+            {
+                Content = "This is a test publication"
+            };
+            var publication =
+                dbContext.Publications.First(p => p.AuthorId.ToString() == "949a14ed-2e82-4f5a-a684-a9c7e3ccb52e");
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<AccessViolationException>(async () =>
+            {
+                await publicationService.UpdatePublicationAsync(dto, publication.Id);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(NotAuthorizedToEditPublication));
+        }
+
+        [Test]
+        public async Task DeletePublicationAsync_ValidId_DeletesPublication()
+        {
+            // Arrange
+            var publication =
+                dbContext.Publications.First(p => p.AuthorId.ToString() == "123400ce-d726-4fc8-83d9-d6b3ac1f591e");
+            var countBefore = dbContext.Publications.Count();
+
+            // Act
+            await publicationService.DeletePublicationAsync(publication.Id);
+
+            // Assert
+            Assert.That(dbContext.Publications.Count(), Is.EqualTo(countBefore - 1));
+        }
+
+        [Test]
+        public async Task DeletePublicationAsync_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var invalidPublicationId = Guid.NewGuid(); // Use a non-existing ID
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<NullReferenceException>(async () =>
+            {
+                await publicationService.DeletePublicationAsync(invalidPublicationId);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(PublicationNotFound));
+        }
+
+        [Test]
+        public async Task DeletePublicationAsync_ThrowsAccessViolationExceptionException()
+        {
+            // Arrange
+            var publication =
+                dbContext.Publications.First(p => p.AuthorId.ToString() == "949a14ed-2e82-4f5a-a684-a9c7e3ccb52e");
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<AccessViolationException>(async () =>
+            {
+                await publicationService.DeletePublicationAsync(publication.Id);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(NotAuthorizedToDeletePublication));
+        }
+
+        [Test]
+        public async Task DeletePublicationAsync_DeletesComments()
+        {
+            // Arrange
+            var publicationId = dbContext.Publications.Last().Id;
+
+            // Act
+            await publicationService.DeletePublicationAsync(publicationId);
+
+            // Assert
+            Assert.IsEmpty(dbContext.Comments.Where(c => c.PublicationId == publicationId));
+        }
+
+        [Test]
+        public async Task CreateCommentAsync_ValidDto_CreatesComment()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var publicationId = dbContext.Publications.First().Id;
+            var countBefore = dbContext.Comments.Count(c => c.PublicationId == publicationId);
+
+            // Act
+            await publicationService.CreateCommentAsync(dto, publicationId);
+
+            // Assert
+            Assert.That(dbContext.Comments.Count(c => c.PublicationId == publicationId), Is.EqualTo(countBefore + 1));
+        }
+
+        [Test]
+        public async Task CreateCommentAsync_ValidDto_CreatesCommentWithCorrectContent()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var publicationId = dbContext.Publications.First().Id;
+
+            // Act
+            await publicationService.CreateCommentAsync(dto, publicationId);
+
+            // Assert
+            Assert.That(dbContext.Comments.Last().Content, Is.EqualTo(dto.Content));
+        }
+
+        [Test]
+        public async Task CreateCommentAsync_InValidPublicationId_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var invalidPublicationId = Guid.NewGuid(); // Use a non-existing ID
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<NullReferenceException>(async () =>
+            {
+                await publicationService.CreateCommentAsync(dto, invalidPublicationId);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(PublicationNotFound));
+        }
+
+        [Test]
+        public async Task UpdateCommentAsync_ValidDtoAndId_UpdatesComment()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var comment =
+                dbContext.Comments.First(c => c.AuthorId.ToString() == "123400ce-d726-4fc8-83d9-d6b3ac1f591e");
+
+            // Act
+            await publicationService.UpdateCommentAsync(dto, comment.Id);
+
+            // Assert
+            Assert.That(comment.Content, Is.EqualTo(dto.Content));
+        }
+
+        [Test]
+        public async Task UpdateCommentAsync_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var invalidCommentId = Guid.NewGuid(); // Use a non-existing ID
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<NullReferenceException>(async () =>
+            {
+                await publicationService.UpdateCommentAsync(dto, invalidCommentId);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(CommentNotFound));
+        }
+
+        [Test]
+        public async Task UpdateCommentAsync_ThrowsAccessViolationExceptionException()
+        {
+            // Arrange
+            var dto = new CommentFormDto()
+            {
+                Content = "This is a test comment"
+            };
+            var comment =
+                dbContext.Comments.First(c => c.AuthorId.ToString() == "123456ed-2e82-4f5a-a684-a9c7e3ccb52e");
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<AccessViolationException>(async () =>
+            {
+                await publicationService.UpdateCommentAsync(dto, comment.Id);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(NotAuthorizedToEditComment));
+        }
+
+        [Test]
+        public async Task DeleteCommentAsync_ValidId_DeletesComment()
+        {
+            // Arrange
+            var comment =
+                dbContext.Comments.First(c => c.AuthorId.ToString() == "123400ce-d726-4fc8-83d9-d6b3ac1f591e");
+            var countBefore = dbContext.Comments.Count();
+
+            // Act
+            await publicationService.DeleteCommentAsync(comment.Id);
+
+            // Assert
+            Assert.That(dbContext.Comments.Count(), Is.EqualTo(countBefore - 1));
+        }
+
+        [Test]
+        public async Task DeleteCommentAsync_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var invalidCommentId = Guid.NewGuid(); // Use a non-existing ID
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<NullReferenceException>(async () =>
+            {
+                await publicationService.DeleteCommentAsync(invalidCommentId);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(CommentNotFound));
+        }
+
+        [Test]
+        public async Task DeleteCommentAsync_ThrowsAccessViolationExceptionException()
+        {
+            // Arrange
+            var comment =
+                dbContext.Comments.First(c => c.AuthorId.ToString() == "123456ed-2e82-4f5a-a684-a9c7e3ccb52e");
+
+            // Act and Assert
+            var exception = Assert.ThrowsAsync<AccessViolationException>(async () =>
+            {
+                await publicationService.DeleteCommentAsync(comment.Id);
+            });
+
+            // Optionally assert on the exception message or other details
+            Assert.That(exception!.Message, Is.EqualTo(NotAuthorizedToDeleteComment));
+        }
+
+        [Test]
+        public async Task GetCommentsOnPublicationAsync_ValidPublicationId_ReturnsComments()
+        {
+            // Arrange
+            var publicationId = Guid.Parse("a0a0a6a0-0b1e-4b9e-9b0a-0b9b9b9b9b9b");
+
+            // Act
+            var result = await publicationService.GetCommentsOnPublicationAsync(publicationId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<CommentDto>>(result);
+
+            // Assuming some comments were seeded during setup
+            Assert.IsTrue(result.Any());
         }
     }
 }
