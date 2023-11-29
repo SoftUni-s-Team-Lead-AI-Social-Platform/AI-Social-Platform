6781d07521d96c903d1287a965e6be1d81e3060a
Bagie <Bagriyan.dilyanov@abv.bg>
2023-11-29 12:35:41 +0200

    Add Base Service for social features, AutoMapper

---
+++ AI-Social-Platform.Services.Data/Interfaces/IBaseSocialService.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace AI_Social_Platform.Services.Data.Interfaces
+{
+    public interface IBaseSocialService<TEntity,TDto> where TDto : class
+    {
+        Task<IEnumerable<TDto>> GetAllAsync(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+        Task<TDto> GetByIdAsync(Guid id);
+        Task CreateAsync(TDto dto);
+        Task UpdateAsync(TDto dto, Guid id);
+        Task DeleteAsync(Guid id);
+    }
+}
