fcd4c8c7f59a95b6d66ef3a64da3e65b9790f3cd
Bagie <Bagriyan.dilyanov@abv.bg>
2023-12-01 18:36:16 +0200

    BaseSocialService

--- AI-Social-Platform.Services.Data/Interfaces/IBaseSocialService.cs
+++ AI-Social-Platform.Services.Data/Interfaces/IBaseSocialService.cs
@@ -2,12 +2,12 @@
 
 namespace AI_Social_Platform.Services.Data.Interfaces
 {
-    public interface IBaseSocialService<TEntity,TDto> where TDto : class
+    public interface IBaseSocialService<TEntity> where TEntity : class
     {
-        Task<IEnumerable<TDto>> GetAllAsync(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
-        Task<TDto> GetByIdAsync(Guid id);
-        Task CreateAsync(TDto dto);
-        Task UpdateAsync(TDto dto, Guid id);
-        Task DeleteAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid entityId);
     }
 }
