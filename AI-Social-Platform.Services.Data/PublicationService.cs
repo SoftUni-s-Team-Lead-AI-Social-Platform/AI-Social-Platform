8bed8d08d0a9c6982523b4effe4d7ca1605d0370
Bagie <Bagriyan.dilyanov@abv.bg>
2023-12-02 04:33:51 +0200

    implement like, share endpoints

--- AI-Social-Platform.Services.Data/PublicationService.cs
+++ AI-Social-Platform.Services.Data/PublicationService.cs
@@ -154,6 +154,106 @@ public class PublicationService : IPublicationService
             .ToListAsync();
     }
 
+    //Like
+    public async Task CreateLikesOnPublicationAsync(Guid publicationId)
+    {
+        var publication = await dbContext.Publications
+            .FirstOrDefaultAsync(p => p.Id == publicationId);
+        var userId = GetUserId();
+
+        if (publication == null)
+        {
+            throw new NullReferenceException(PublicationNotFound);
+        }
+
+        var like = new Like
+        {
+            PublicationId = publicationId,
+            UserId = userId
+        };
+
+        await dbContext.Likes.AddAsync(like);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteLikeOnPublicationAsync(Guid likeId)
+    {
+        var like = await dbContext.Likes.FirstOrDefaultAsync(l => l.Id == likeId);
+        var userId = GetUserId();
+
+        if (like == null)
+        {
+            throw new NullReferenceException(LikeNotFound);
+        }
+
+        if (like.UserId != userId)
+        {
+            throw new AccessViolationException(NotAuthorizedToDeleteLike);
+        }
+
+        dbContext.Likes.Remove(like);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<LikeDto>> GetLikesOnPublicationAsync(Guid publicationId)
+    {
+        return await dbContext.Likes
+            .Where(l => l.PublicationId == publicationId)
+            .ProjectTo<LikeDto>(mapper.ConfigurationProvider)
+            .OrderByDescending(l => l.DateCreated)
+            .ToListAsync();
+    }
+
+    //Share
+    public async Task CreateSharesOnPublicationAsync(Guid publicationId)
+    {
+        var publication = await dbContext.Publications
+            .FirstOrDefaultAsync(p => p.Id == publicationId);
+        var userId = GetUserId();
+
+        if (publication == null)
+        {
+            throw new NullReferenceException(PublicationNotFound);
+        }
+
+        var share = new Share
+        {
+            PublicationId = publicationId,
+            UserId = userId
+        };
+
+        await dbContext.Shares.AddAsync(share);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteShareOnPublicationAsync(Guid shareId)
+    {
+        var share = await dbContext.Shares.FirstOrDefaultAsync(s => s.Id == shareId);
+        var userId = GetUserId();
+
+        if (share == null)
+        {
+            throw new NullReferenceException(ShareNotFound);
+        }
+
+        if (share.UserId != userId)
+        {
+            throw new AccessViolationException(NotAuthorizedToDeleteShare);
+        }
+
+        dbContext.Shares.Remove(share);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<ShareDto>> GetSharesOnPublicationAsync(Guid publicationId)
+    {
+        return await dbContext.Shares
+            .Where(s => s.PublicationId == publicationId)
+            .ProjectTo<ShareDto>(mapper.ConfigurationProvider)
+            .OrderByDescending(s => s.DateCreated)
+            .ToListAsync();
+    }
+
     private Guid GetUserId()
     {
         var userId = httpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
