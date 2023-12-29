1c3a568ba5461f84126fd377b57f13dde1d0edd4
ioliuni <start.expert@abv.bg>
2023-12-29 09:58:14 +0200

    Прехвърляне на промените от User-Profile

--- ai-social-platform.client/src/components/Userprofile/Userprofile.jsx
+++ ai-social-platform.client/src/components/Userprofile/Userprofile.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import styles from "./Userprofile.css";
 import * as userService from "../../core/services/userService";
 import { Link, useParams } from "react-router-dom";
 import { PATH } from "../../core/environments/costants";
 import { pathToUrl } from "../Userprofile/pathUtils";
+import AuthContext from "../../contexts/authContext";
 
 export default function Userprofile() {
   const { userId } = useParams();
 
   const [userData, setUserData] = useState(null);
+  const [authUserData, setAuthUserData] = useState(null);
   const [error, setError] = useState(null);
+  const authContext = useContext(AuthContext);
 
   useEffect(() => {
     userService
@@ -18,7 +21,13 @@ export default function Userprofile() {
         setUserData(result);
       })
       .catch((error) => setError(error));
-  }, [userId]);
+    userService
+      .getUserDetails(authContext.userId)
+      .then((result) => {
+        setAuthUserData(result);
+      })
+      .catch((error) => setError(error));
+  }, [userId, authContext.userId]);
 
   if (error) {
     return <div>{error}</div>;
@@ -27,6 +36,50 @@ export default function Userprofile() {
   if (!userData) {
     return <div>Loading...</div>;
   }
+
+  const date = new Date(userData.birthday);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const formattedBirthday = `${day < 10 ? "0" : ""}${day}/${
+    month < 10 ? "0" : ""
+  }${month}/${year}`;
+
+  console.log("userData", userData);
+  console.log("authUserData", authUserData);
+  let isCurrentUserProfile = userId === authContext.userId;
+  let isUserFriend = true;
+  if (isCurrentUserProfile) {
+    isUserFriend = true;
+  } else {
+    isUserFriend = authUserData.friends.some(
+      (friend) => friend.id === userData.userId
+    );
+  }
+  isCurrentUserProfile = false;
+  isUserFriend = false;
+  console.log("isUserFriend", isUserFriend);
+  const handleAddFriend = async () => {
+    try {
+      await userService.addFriend(userId);
+      // Промени състоянието, че сега потребителят е приятел
+      // setIsFriend(true);
+      // Извикване на нова заявка, за да актуализира информацията за приятелите
+      // const authUserResult = await userService.getUserDetails(authContext.userId);
+      // setAuthUserData(authUserResult);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const handleRemoveFriend = async () => {};
+  //const isCurrentUserProfile = false;
+
+  console.log("userData", userData);
+  console.log("authUserData", authUserData);
+  //console.log("authContext", authContext);
+  console.log(userId, authContext.userId);
+  console.log(isCurrentUserProfile);
 
   return (
     <div className="user-profile">
@@ -43,43 +96,61 @@ export default function Userprofile() {
             alt="User profile pic"
           />
           <div className="user-info-text">
+            <p className="cover-profile">User Profile</p>
+
             <p className="username-profile">
               {userData.firstName} {userData.lastName}
             </p>
             <p className="posted-user">
               E-mail:
-              <a href="mailto: {userData?.email}"> {userData?.userName}</a>
+              <a href="mailto: {userData?.userName}"> {userData?.userName}</a>
             </p>
-            <p>GSM: {userData.phoneNumber}</p>
+            <p className="posted-user">GSM: {userData.phoneNumber}</p>
+            <p className="posted-user">Country: {userData.country}</p>
+            <p className="posted-user">State: {userData.state}</p>
+            <p className="posted-user">Gender: {userData.gender}</p>
+            <p className="posted-user">School: {userData.school}</p>
+            <p className="posted-user">Birthday: {formattedBirthday}</p>
+            <p className="posted-user">
+              Relationship Status: {userData.relationship}
+            </p>
+            <div>
+              <p className="posted-user">Friends</p>
+              <ul>
+                {userData.friends.map((friend) => (
+                  <li key={friend.id}>
+                    <Link to={PATH.userProfile(friend.id)}>
+                      {friend.firstName} {friend.lastName}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
 
           <div className="edit">
-            <Link to={pathToUrl(PATH.profileedit, { userId: userId })}>
-              <img
-                className="edit-img"
-                src="../../../public/images/edit-pen-icon-6.jpg"
-                alt="edit button"
-              />
-            </Link>
+            {isCurrentUserProfile && (
+              <Link to={pathToUrl(PATH.profileedit, { userId: userId })}>
+                <img
+                  className="edit-img"
+                  src="../../../public/images/edit-pen-icon-6.jpg"
+                  alt="edit button"
+                />
+              </Link>
+            )}
+            {!isCurrentUserProfile && (
+              <>
+                {isUserFriend ? (
+                  <button onClick={handleRemoveFriend}>
+                    Remove from Friends
+                  </button>
+                ) : (
+                  <button onClick={handleAddFriend}>Add as Friend</button>
+                )}
+              </>
+            )}
           </div>
         </div>
-        <section className="content-description">
-          <p>
-            City, Country: {userData.state}, {userData.country}
-          </p>
-          <p>Schools: {userData.userSchools}</p>
-          <p>
-            Birthday: {userData.birthday}, Gender: {userData.gender}
-          </p>
-          <p>Relationship Status: {userData.relationship}</p>
-
-          <p>
-            <a href="#">Publications</a>
-          </p>
-          <p>
-            <a href="#">Friends</a>
-          </p>
-        </section>
       </article>
     </div>
   );
--- ai-social-platform.client/src/components/Userprofile/Userprofileedit.jsx
+++ ai-social-platform.client/src/components/Userprofile/Userprofileedit.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styles from "./Userprofile.css";
 import * as userService from "../../core/services/userService";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useFormik } from "formik";
+import { useFormik, Field } from "formik";
 import { PATH, ProfileFormKeys } from "../../core/environments/costants";
 import userProfileValidation from "./userProfileValidation";
 
@@ -13,8 +13,9 @@ export default function Userprofileedit() {
 
   useEffect(() => {
     userService
-      .getUserData(userId)
+      .getUserDetails(userId)
       .then((result) => {
+        console.log(result);
         setUserData(result);
       })
       .catch((error) => console.log(error));
@@ -23,23 +24,38 @@ export default function Userprofileedit() {
     return <div>Loading...</div>;
   }
 
+  const date = new Date(userData.birthday);
+
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const year = date.getFullYear();
+
+  const formattedDate = `${year}-${month}-${day}`;
+
   const initialValues = {
     [ProfileFormKeys.FirstName]: userData.firstName,
     [ProfileFormKeys.LastName]: userData.lastName,
-    [ProfileFormKeys.Email]: "test@test.com",
-    [ProfileFormKeys.PhoneNumber]: "12345678",
-    [ProfileFormKeys.Country]: "bgbg",
-    [ProfileFormKeys.State]: "sfsf",
-    [ProfileFormKeys.Gender]: 1,
-    [ProfileFormKeys.School]: "abc",
-    [ProfileFormKeys.Birthday]: "2023-12-17T16:29:38.153Z",
-    [ProfileFormKeys.Relationship]: 1,
-    [ProfileFormKeys.Schools]: [
-      {
-        name: "string",
-        state: "string",
-      },
-    ],
+    [ProfileFormKeys.PhoneNumber]: userData.phoneNumber,
+    [ProfileFormKeys.ProfilePicture]: userData.profilePictureBase64,
+    [ProfileFormKeys.CoverPhoto]: userData.coverPhotoBase64,
+    [ProfileFormKeys.Country]: userData.country,
+    [ProfileFormKeys.State]: userData.state,
+    [ProfileFormKeys.Gender]:
+      userData.gender === "Man"
+        ? "0"
+        : userData.gender === "Woman"
+        ? "1"
+        : userData.gender,
+    [ProfileFormKeys.School]: userData.school,
+    [ProfileFormKeys.Birthday]: formattedDate,
+    [ProfileFormKeys.Relationship]:
+      userData.relationship === "Single"
+        ? "0"
+        : userData.relationship === "InARelationship"
+        ? "1"
+        : userData.relationship === "Married"
+        ? "2"
+        : userData.relationship,
   };
 
   const {
@@ -53,57 +69,38 @@ export default function Userprofileedit() {
   } = useFormik({
     initialValues,
     enableReinitialize: true,
-    onSubmit,
     validationSchema: userProfileValidation,
+    onSubmit,
   });
 
-  const updateSubmitHandler = async ({
-    firstName,
-    lastName,
-    phoneNumber,
-    country,
-    state,
-    gender,
-    school,
-    birthday,
-    relationship,
-    schools,
-  }) => {
-    await userService.update({
-      firstName,
-      lastName,
-      phoneNumber,
-      country,
-      state,
-      gender,
-      school,
-      birthday,
-      relationship,
-      schools,
-    });
-
-    // setAuth(result);
-
-    // localStorage.setItem('accessToken', result.token);
-
-    useNavigate(PATH.home);
-  };
-
   async function onSubmit(values) {
-    debugger;
+    values = {
+      ...values,
+      [ProfileFormKeys.Gender]: parseInt(values[ProfileFormKeys.Gender], 10),
+      [ProfileFormKeys.Relationship]: parseInt(
+        values[ProfileFormKeys.Relationship],
+        10
+      ),
+    };
     try {
-      await updateSubmitHandler(values);
+      console.log("values", values);
+      //await updateSubmitHandler(values);
+      await userService.update(values);
     } catch (error) {
       console.log("Error:", error);
     }
   }
+  useNavigate(PATH.home);
   return (
     <div className="user-profile">
       <article className="post-item">
         <form onSubmit={handleSubmit}>
           <img
             className="user-cover"
-            src="../../../public/images/iceage.png"
+            src={
+              values[ProfileFormKeys.CoverPhoto] ||
+              "../../../public/images/Logo.png"
+            }
             alt="User cover photo"
           />
           <div className="user-info-wrapper">
@@ -114,13 +111,41 @@ export default function Userprofileedit() {
             />
             <div className="user-info-text">
               <div className="username-profile">
-                <label htmlFor={ProfileFormKeys.FirstName}></label>
+                <label htmlFor={ProfileFormKeys.ProfilePicture}>
+                  Change the ProfilePicture
+                </label>
+
+                <input
+                  type="text"
+                  id={ProfileFormKeys.ProfilePicture}
+                  name={ProfileFormKeys.ProfilePicture}
+                  placeholder="Upload a photo..."
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values[ProfileFormKeys.ProfilePicture]}
+                />
+                <div>
+                  <label htmlFor={ProfileFormKeys.CoverPhoto}>
+                    Change the CoverPhoto
+                  </label>
+
+                  <input
+                    type="text"
+                    id={ProfileFormKeys.CoverPhoto}
+                    name={ProfileFormKeys.CoverPhoto}
+                    placeholder="Upload a photo..."
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.CoverPhoto]}
+                  />
+                </div>
+                <label htmlFor={ProfileFormKeys.FirstName}>First Name </label>
                 <input
                   className={
                     errors[ProfileFormKeys.FirstName] &&
                     touched[ProfileFormKeys.FirstName]
-                      ? styles["name-input-error"]
-                      : styles["name-input"]
+                      ? "name-input-error"
+                      : "name-input"
                   }
                   type="text"
                   name={ProfileFormKeys.FirstName}
@@ -133,13 +158,13 @@ export default function Userprofileedit() {
 
                 {errors[ProfileFormKeys.FirstName] &&
                   touched[ProfileFormKeys.FirstName] && (
-                    <p className={styles["error-message"]}>
-                      {errors[RegisterFormKeys.FirstName]}
+                    <p className="error-message">
+                      {errors[ProfileFormKeys.FirstName]}
                     </p>
                   )}
 
                 <div className={styles["last-name"]}>
-                  <label htmlFor={ProfileFormKeys.LastName}></label>
+                  <label htmlFor={ProfileFormKeys.LastName}>Last Name </label>
                   <input
                     className={
                       errors[ProfileFormKeys.LastName] &&
@@ -157,71 +182,229 @@ export default function Userprofileedit() {
 
                   {errors[ProfileFormKeys.LastName] &&
                     touched[ProfileFormKeys.LastName] && (
-                      <p className={styles["error-message"]}>
+                      <p className="error-message">
                         {errors[ProfileFormKeys.LastName]}
                       </p>
                     )}
                 </div>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.PhoneNumber}>
+                    Phone Number{" "}
+                  </label>
+                  <input
+                    className={
+                      errors[ProfileFormKeys.PhoneNumber] &&
+                      touched[ProfileFormKeys.PhoneNumber]
+                        ? "name-input-error"
+                        : "name-input"
+                    }
+                    type="text"
+                    name={ProfileFormKeys.PhoneNumber}
+                    id={ProfileFormKeys.PhoneNumber}
+                    placeholder="Phone number"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.PhoneNumber]}
+                  />
+
+                  {errors[ProfileFormKeys.PhoneNumber] &&
+                    touched[ProfileFormKeys.PhoneNumber] && (
+                      <p className="error-message">
+                        {errors[ProfileFormKeys.PhoneNumber]}
+                      </p>
+                    )}
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.Country}>Country </label>
+                  <input
+                    className={
+                      errors[ProfileFormKeys.Country] &&
+                      touched[ProfileFormKeys.Country]
+                        ? "name-input-error"
+                        : "name-input"
+                    }
+                    type="text"
+                    name={ProfileFormKeys.Country}
+                    id={ProfileFormKeys.Country}
+                    placeholder="Country"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.Country]}
+                  />
+
+                  {errors[ProfileFormKeys.Country] &&
+                    touched[ProfileFormKeys.Country] && (
+                      <p className="error-message">
+                        {errors[ProfileFormKeys.Country]}
+                      </p>
+                    )}
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.State}>State </label>
+                  <input
+                    className={
+                      errors[ProfileFormKeys.State] &&
+                      touched[ProfileFormKeys.State]
+                        ? "name-input-error"
+                        : "name-input"
+                    }
+                    type="text"
+                    name={ProfileFormKeys.State}
+                    id={ProfileFormKeys.State}
+                    placeholder="State"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.State]}
+                  />
+
+                  {errors[ProfileFormKeys.State] &&
+                    touched[ProfileFormKeys.State] && (
+                      <p className="error-message">
+                        {errors[ProfileFormKeys.State]}
+                      </p>
+                    )}
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.Gender}>Gender </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name={ProfileFormKeys.Gender}
+                      id={ProfileFormKeys.Gender}
+                      value="0"
+                      checked={values[ProfileFormKeys.Gender] === "0"}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    Man{" "}
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name={ProfileFormKeys.Gender}
+                      id={ProfileFormKeys.Gender}
+                      value="1"
+                      checked={values[ProfileFormKeys.Gender] === "1"}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    Woman
+                  </label>
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.School}>School </label>
+                  <input
+                    className={
+                      errors[ProfileFormKeys.School] &&
+                      touched[ProfileFormKeys.School]
+                        ? "name-input-error"
+                        : "name-input"
+                    }
+                    type="text"
+                    name={ProfileFormKeys.School}
+                    id={ProfileFormKeys.School}
+                    placeholder="School"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.School]}
+                  />
+
+                  {errors[ProfileFormKeys.School] &&
+                    touched[ProfileFormKeys.School] && (
+                      <p className="error-message">
+                        {errors[ProfileFormKeys.School]}
+                      </p>
+                    )}
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.Birthday}>Birthday </label>
+                  <input
+                    className={
+                      errors[ProfileFormKeys.Birthday] &&
+                      touched[ProfileFormKeys.Birthday]
+                        ? "name-input-error"
+                        : "name-input"
+                    }
+                    type="date"
+                    name={ProfileFormKeys.Birthday}
+                    id={ProfileFormKeys.Birthday}
+                    placeholder="Birthday"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values[ProfileFormKeys.Birthday]}
+                  />
+
+                  {errors[ProfileFormKeys.Birthday] &&
+                    touched[ProfileFormKeys.Birthday] && (
+                      <p className="error-message">
+                        {errors[ProfileFormKeys.Birthday]}
+                      </p>
+                    )}
+                </section>
+
+                <section className={styles["phone-number-wrapper"]}>
+                  <label htmlFor={ProfileFormKeys.Relationship}>
+                    Relationship{" "}
+                  </label>
+                  <li>
+                    <label>
+                      <input
+                        type="radio"
+                        name={ProfileFormKeys.Relationship}
+                        id={ProfileFormKeys.Relationship}
+                        value="0"
+                        checked={values[ProfileFormKeys.Relationship] === "0"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      Single
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      <input
+                        type="radio"
+                        name={ProfileFormKeys.Relationship}
+                        id={ProfileFormKeys.Relationship}
+                        value="1"
+                        checked={values[ProfileFormKeys.Relationship] === "1"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      InARelationship
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      <input
+                        type="radio"
+                        name={ProfileFormKeys.Relationship}
+                        id={ProfileFormKeys.Relationship}
+                        value="2"
+                        checked={values[ProfileFormKeys.Relationship] === "2"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      Married
+                    </label>
+                  </li>
+                </section>
+                <button
+                  type="submit"
+                  className={styles["register-button"]}
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
               </div>
             </div>
-            <section className={styles["phone-number-wrapper"]}>
-              <label htmlFor={ProfileFormKeys.PhoneNumber}></label>
-              <input
-                className={
-                  errors[ProfileFormKeys.PhoneNumber] &&
-                  touched[ProfileFormKeys.PhoneNumber]
-                    ? styles["input-field-error"]
-                    : styles["input-field"]
-                }
-                type="text"
-                name={ProfileFormKeys.PhoneNumber}
-                id={ProfileFormKeys.PhoneNumber}
-                placeholder="Phone number"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values[ProfileFormKeys.PhoneNumber]}
-              />
-
-              {errors[ProfileFormKeys.Email] &&
-                touched[ProfileFormKeys.Email] && (
-                  <p className={styles["error-message"]}>
-                    {errors[ProfileFormKeys.Email]}
-                  </p>
-                )}
-            </section>
-            <section className={styles["phone-number-wrapper"]}>
-              <label htmlFor={ProfileFormKeys.Country}></label>
-              <input
-                className={
-                  errors[ProfileFormKeys.Country] &&
-                  touched[ProfileFormKeys.Country]
-                    ? styles["input-field-error"]
-                    : styles["input-field"]
-                }
-                type="text"
-                name={ProfileFormKeys.Country}
-                id={ProfileFormKeys.Country}
-                placeholder="Country"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values[ProfileFormKeys.Country]}
-              />
-
-              {errors[ProfileFormKeys.Country] &&
-                touched[ProfileFormKeys.Country] && (
-                  <p className={styles["error-message"]}>
-                    {errors[ProfileFormKeys.Country]}
-                  </p>
-                )}
-            </section>
           </div>
-          <button
-            type="submit"
-            className={styles["register-button"]}
-            disabled={isSubmitting}
-          >
-            Update
-          </button>
         </form>
       </article>
     </div>
