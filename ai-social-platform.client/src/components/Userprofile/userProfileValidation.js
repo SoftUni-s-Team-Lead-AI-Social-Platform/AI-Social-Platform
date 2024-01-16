e669edbd219da254fd9e842171be32b54a3a79ca
ioliuni <start.expert@abv.bg>
2024-01-16 12:21:58 +0200

    Add Validation for School

--- ai-social-platform.client/src/components/Userprofile/userProfileValidation.js
+++ ai-social-platform.client/src/components/Userprofile/userProfileValidation.js
@@ -6,12 +6,7 @@ const passwordRulse = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}/;
 // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 
 const userProfileValidation = Yup.object({
-    // [ProfileFormKeys.FirstName]: Yup.string()
-    // .required('First name is required. test'),
-    
-    // [ProfileFormKeys.LastName]: Yup.string()
-    // .required('Last name is required.'),
-    
+        
     [ProfileFormKeys.FirstName]: Yup.string()
     .required('First name is required.')
     .min(1, 'First name must be at least 1 characters.')
@@ -34,9 +29,9 @@ const userProfileValidation = Yup.object({
     .min(3, 'State must be at least 3 characters.')
     .max(50, 'State must be at most 50 characters.'),
     
-    // [ProfileFormKeys.School]: Yup.string()
-    // .min(3, 'Last name must be at least 3 characters.')
-    // .max(50, 'Last name must be at most 50 characters.'),
+    [ProfileFormKeys.School]: Yup.string()
+    .min(3, 'Last name must be at least 3 characters.')
+    .max(50, 'Last name must be at most 50 characters.'),
 });
 
 export default userProfileValidation;
